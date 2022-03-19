33d0108088dc79e923d61450f792331aa2f7c313
altynaikemelova <altynai.ar232509@gmail.com>
2022-03-19 18:49:12 +0300

    set static and media urls and roots

--- homework/views.py
+++ homework/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def homepage(request):
-    return HttpResponse("Добро пожаловать в приложение ToDo Admin! ")
+    return render(request," index.html")
 
 
 def test(request):
